267c435a844cde77b3ed3152a7e2102866860c73
sriganesh <sriganeshlokesh@gmail.com>
2020-08-15 14:27:59 -0500

    Request URL Change

--- client/src/components/core/ProductImage.js
+++ client/src/components/core/ProductImage.js
@@ -4,7 +4,7 @@ const ProductImage = ({ item, url }) => (
   <div className="product_image text-center">
     <img
       className="mb-1 "
-      src={`${process.env.REACT_APP_API}/${url}/photo/${item}`}
+      src={`/api/${url}/photo/${item}`}
       alt={item.name}
       style={{
         maxWidth: "95%",
